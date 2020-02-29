3bf6184e6adcb991608489e1d2071fd5dba63588
Jayque17 <julien.leon99@gmail.com>
2020-02-29 20:43:56 +0100

    Aération du code, docstrings refaites et changement du nom de plusieurs variables.

--- TheRPG.py
+++ TheRPG.py
@@ -1,65 +1,83 @@
+#_____Modules_____
+
 from random import randint
 import time
 
+
+
+#_____Classes_____
+
 class Slime:
-    """
-    Classe qui représente un slime.
-    """
     def __init__(self):
-        self.name = "Slime"
-        self.life = 5
-        self.strengh = 2
+        """Constructeur de la classe Slime"""
+
+        self.name = "Slime"  #Nom
+        self.life = 5  #Points de vie
+        self.strengh = 2  #Points de force (= puissance d'attaque)
 
 
 class Hero:
-    """
-    Classe représentant le personnage.
-    """
     def __init__(self):
-        self.gender = askGender()
-        self.name = askName(self.gender)
-        self.level = 1
-        self.experience = 0
-        self.life = 10
-        self.mana = 5
-        self.strengh = 1
-        self.intelligence = 1
-        self.chance = 1
-        self.vitality = 1
-        self.precision = 1
+        """Constructeur de la classe Hero"""
 
-    def manageStat(self, stat_pt):
+        self.name = askName()  #Nom
+        self.level = 1  #Niveau
+        self.experience = 0  #Expérience
+        self.life = 10  #Points de vie
+        self.mana = 5  #Points de magie
+        self.strengh = 1  #Points de force (= puissance d'attaque)
+        self.intelligence = 1  #Points d' intelligence, augmente la chance de caster un sort
+        self.chance = 1  #Points de chance, augmente la chance sur un lancé de dé hors combat
+        self.vitality = 1  #Points de vitalité, défense naturelle du personnage
+        self.precision = 1  #Points de précision, augmente la chance de faire une attaque physique
+
+
+    def manageStat(self, tal_point):
+        """Fonction qui permet d'améliorer les statistiques de notre personnage.
+
+        self (Hero)
+        tal_point (entier) : points de statistique à attribuer 
         """
-        Fonction qui permet d'ameiliorer les statistiques de notre personnage.
-        """
-        while stat_pt > 0:
-            print("Tu as", stat_pt,"points à placer")
-            answer = grepAnswer()
-            if answer == "for":
-                self.strengh += 1
+
+        while tal_point > 0:  #La boucle finit quand il n'y a plus de points de statistique à attribuer
+            
+            print("Tu as", tal_point,"points à placer")
+            answer = grepAnswer()  #Récupération du choix de l'utilisateur
+            
+            if answer == "for":  #Amélioration de la force
+
+                self.strengh += 1  #Incrémentation de la statistique
                 print("+1 en Force\n")
-                stat_pt -= 1
-            if answer == "int":
+                tal_point -= 1  #Décrémentation du nombre de tal_point
+
+            if answer == "int":  #Amélioration de l'intelligence
+
                 self.intelligence += 1
                 print("+1 en Intelligence\n")
-                stat_pt -= 1
-            if answer == "cha":
+                tal_point -= 1
+
+            if answer == "cha":  #Amélioration de la chance
+
                 self.chance += 1
                 print("+1 en Chance\n")
-                stat_pt -= 1
-            if answer == "vit":
+                tal_point -= 1
+
+            if answer == "vit":  #Amélioration de la vitalité
+
                 self.vitality += 1
                 print("+1 en Vitalité\n")
-                stat_pt -= 1
-            if answer == "pre":
+                tal_point -= 1
+
+            if answer == "pre":  #Amélioration de la précision
+
                 self.precision += 1
                 print("+1 en Précision\n")
-                stat_pt -= 1
+                tal_point -= 1
     
+
     def showStat(self):
-        """
-        La fonction montre les statistiques du personage.
-        """
+        """La fonction affiche les statistiques du personnage."""
+
         print("Force :", self.strengh)
         print("Intelligence :", self.intelligence)
         print("Chance :", self.chance)
@@ -69,178 +87,208 @@ class Hero:
         print("Magie :", "@ "*self.mana)
         print("Niveau :", self.level)
         print("Xp :", self.experience, "\n")
-            
+
+
+
+#_____Fonctions_____
 
 def grepAnswer():
-    """
-    La fonction collecte les entrées clavier du joueur.
-    """
+    """La fonction récupère les entrées au clavier, puis les renvoies."""
+
     answer = input()
+
     return answer
 
-def askGender():
-    """
-    La fonction pose la question homme ou femme.
-    Elle renvoie le genre du personage.
-    """
-    while(1):
-        print("Bonjour à toi aventurier, avant de commencer ton époppé présente toi.Est-tu un homme ou une femme? [H/F]")
-        gender = grepAnswer()
-        if gender == "F" or gender == "H":
-            return gender
 
-def askName(gender):
-    """
-    La fonction demande le prénom du personage.
-    Elle renvoie le prenom du personage.
-    gender : chaîne de caractère.
-    """
+def askName():
+    """La fonction demande le prénom du personage, puis le renvoie."""
 
-    if gender == "F":
-        print("Si tu es prête à devenir une légende dit moi ton nom FEMME!")
-        name = grepAnswer()
-    elif gender == "H":
-        print("Si tu es prêt à devenir une légende dit moi ton nom CHIEN!")
-        name = grepAnswer()
+    print("Si tu es prêt à devenir une légende dit moi ton nom CHIEN!")
+    name = grepAnswer()
+
     return name
-    
+
+
 def introStat():
-    """
-    La fonction présente les diffenrentes statistique du joueur.
-    Elle renvoie 5.
-    """
+    """La fonction présente les diffenrentes statistiques du joueur, puis renvoie des points de talent (= tal_point)."""
+
     print("Bon par défaut t'as 1 partout de vraies statistiques de gros noob.")
-    print("Je suis gentil je t'offre 5 points de talent, places les judicieusement.")
+    
     print("Tes statistiques sont les suivantes :")
     print("Force (for), ça sert à casser des bouches plus fort.")
     print("Intelligence (int), pratique pour augmenter la puissance du barbeuc de mob.")
     print("Chance (cha), ça évite que le dé de la DESTINY OF DOOM APOCALYPTIQUE te bolosse de trop.")
     print("Vitalité (vit), plus t'en as moins t'as mal.")
     print("Précision (pre), avec ça tu deviens le roi des sniper.\n")
-    return 5
+    
+    print("Je suis gentil je t'offre 5 points de talent, places les judicieusement.")
+    tal_point = 5
+
+    return tal_point
+
 
 def rollTheDice():
-    """
-    Fait un lancer de dé 100.
-    """
+    """Émule un lancer de dé 100."""
+
     return randint(1, 101)
 
+
 def statOnDice(dice, stat, op):
-    """
-    Modifie la valeur d'un dé en fonction d'une statistique du perso.
-    renvoie la valeur de dé modifié.
-    dice: entier
-    stat: entier
-    op: + ou -
+    """Modifie la valeur d'un dé en fonction d'une statistique, puis renvoie la valeur du dé modifié.
+    
+    dice (entier) : un dé
+    stat (entier) : statistique (= force, chance, intelligence, etc...)
+    op (chaîne de caractère) : "+" ou "-", change par rapport à l'action du dé
+   
     >>> statOnDice(56, 18, "+")
     66
     >>> statOnDice(95, 18, "-")
     78
     """
+
     dice = float(dice)
     impact = dice/100 * float(stat)
+
     if op == "+":
+
         dice += impact
+
     if op == "-":
+
         dice -= impact
+
     return round(dice)
 
 
-
 def initiative(player, monster):
+    """La fonction détermine qui du joueur ou du monstre à l'initiative. Elle renvoie True pour le joueur, False sinon.
+
+    player (classe)
+    monster (classe)
     """
-    Renvoie True si le joueur réussi le jet d'initiave.
-    False si c'est le monstre.
-    """
-    #jet de dé sans les stats joueur/monstre.
-    player_dice = rollTheDice()
-    print("Jet de", player.name,":", player_dice)
-    monster_dice = rollTheDice()
+
+    player_dice = rollTheDice()  #Lancé de dé du joueur
+    monster_dice = rollTheDice()  #Lancé de dé du monstre
+    player_dice = statOnDice(player_dice, player.chance, "+")  #Ajout de la statistique de chance sur le dé du joueur
     
-    #ajout des stats joueur sur son lancé.
-    player_dice = statOnDice(player_dice, player.chance, "+")
     print("Jet de", player.name,":", player_dice)
     print("Jet de", monster.name,":", monster_dice, "\n")
-    if monster_dice > player_dice:
-        return False
-    return True
+    
+    if monster_dice > player_dice:  #Comparaison du score des deux dés
+    
+        return False  #Dé du monstre qui à le plus gros score
+    
+    return True  #Dé du joueur qui à le plus gros score ou égalité entre les deux dés.
+
 
 def attack(turn, player, monster):
+    """La fonction gère le tour d'attaque en fonction du monstre ou du joueur, puis renvoie True si c'est le tour du monstre, False si c'est celui du joueur.
+    
+    turn (booléen) : Représente soit le tour du monstre soit celui du joueur.
+    player (classe)
+    monster (classe)
     """
-    gère le tour d'attaque en fonction du monstre ou du joueur.
-    renvoie True si c'est le tour du monstre, False si c'est celui du joueur.
-    turn : booléen qui représente soit le tour du monstre soit celui du joueur.
-    """
-    if turn == True:
+
+    if turn == True:  #Tour du joueur
+
         print(player.name, "prépares ton attaque")
-        attack_dice = rollTheDice()
+        attack_dice = rollTheDice()  #Lancement du dé qui détermine si le joueur réussi à lancer une attaque
         print(player.name, "tu as fait un", attack_dice)
-        attack_dice = statOnDice(attack_dice, player.precision, "-")
+        attack_dice = statOnDice(attack_dice, player.precision, "-")  #Augmentation de la précision de l'attaque, de la chance qu'elle soit éxécutée
         print(player.name, "tu as fait un", attack_dice)
-        if attack_dice <= 50:
+        
+        if attack_dice <= 50:  #L'attaque est lancée
+        
             print("lancé réussi")
-            monster.life -= player.strengh
+            monster.life -= player.strengh  #Le monstre reçoit autant de dégat que le joueur a de force
             print("PV", monster.name, ":", monster.life, "\n")
-        else:
+        
+        else:  #L'attaque échoue
+        
             print("lancé échoué\n")
-        return False
-    elif turn == False:
+        
+        return False  #Au tour du monstre de tenter une attaque
+    
+    elif turn == False:  #Tour du monstre
+    
         print(monster.name, "prépare son attaque")
-        attack_dice = rollTheDice()
+        attack_dice = rollTheDice()  #Lancement du dé qui détermine si le monstre réussi à lancer une attaque
         print(monster.name, "a fait un", attack_dice)
-        if attack_dice <= 50:
+    
+        if attack_dice <= 50:  #L'attaque est lancée
+    
             print("lancé réussi")
-            if player.vitality < monster.strengh:
-                attack_monster = monster.strengh - player.vitality
-                player.life -= attack_monster
+    
+            if player.vitality < monster.strengh:  #Si l'attaque du monstre est plus haute que la vitalité du joueur
+    
+                attack_monster = monster.strengh - player.vitality   #L'attaque du monstre est égale à la force du monstre moins la vitalité du joueur
+                player.life -= attack_monster  #L'attaque du monstre détermine le nombre de dégat mis au joueur
                 print("PV :", player.life, "\n")
-            else:
+    
+            else:  #La défense du joueur est trop haute, le joueur ne reçoit pas de dégat
+    
                 print(monster.name, "ne vous a fait aucun dégat.")  
-        else:
+    
+        else:  #L'attaque échoue
+    
             print("lancé échoué\n")
-        return True
+    
+        return True #Au tour du joueur de tenter une attaque
+
 
 def fight(player, monster):
+    """La fonction gère le déroulé d'un combats entre le joueur et un monstre, puis renvoie le nom du vainqueur.
+    
+    player (classe)
+    monster (classe)
     """
-    La fonction gère les combats entre le joueur et un monstre.
-    renvoie le vainqueur.
-    """
-    turn = initiative(player, monster)
-    while player.life > 0 or monster.life > 0 :
-        turn = attack(turn, player, monster)
-        if player.life <= 0:
-            return monster.name
-        elif monster.life <= 0:
-            return player.name
-        
+
+    turn = initiative(player, monster)  #Détermine qui commence à attaquer
+    
+    while player.life > 0 or monster.life > 0 :  #Tant qu'il n'en reste pas qu'un
+    
+        turn = attack(turn, player, monster)  #Le combat au tour par tour commence
+    
+        if player.life <= 0: #Si le joueur meurt
+    
+            return monster.name  #On renvoie le nom du monstre
+    
+        elif monster.life <= 0:  #Si le monstre meurt
+    
+            return player.name  #On renvoie le nom du joueur
+
+
 def winFight(winner, player):
-    """
-    Affiche qui à gagner le combat.
-    """
+    """Affiche qui a gagné le combat."""
+
     print(winner, "a gagné le combat")
+
     if winner == player.name:
+
         print("Bravo")
 
+
 def loseFight(winner, player):
+    """Affiche si le personnage à perdu le combat.
+    
+    winner (chaîne de caractère) : Le nom de celui qui à gagné le combat
+    player (classe)
     """
-    Affiche si le personnage à perdu le combat
-    winner : chaîne de caractère.
-    """
-    if winner != player.name:
-        print("Bouhhh")
+
+    if winner != player.name:  #Si le gagnat n'est pas le joueur
+
+        print("Bouhhh")  #SHAME ON YOU
         
 
-    
-    
-
+#_____Main_____
 
 if __name__=="__main__":
 
     player = Hero()
     
-    stat_pt = introStat()
+    tal_point = introStat()
     
-    player.manageStat(stat_pt)
+    player.manageStat(tal_point)
     
     player.showStat()
 
